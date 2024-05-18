b3f744406e87f23d725b4d557d7b0ffe827218b5
SUMIT <sumitspatil205@gmail.com>
2024-05-18 23:19:32 +0530

    18/5/24

--- lib/RegisterPage/ID_Page.dart
+++ lib/RegisterPage/ID_Page.dart
@@ -17,7 +17,7 @@ class _IdPageState extends State<IdPage> {
       ),
       body: Center(
         child: Container(
-          height: 400,
+          height: 500,
           width: 350,
           decoration: BoxDecoration(
             color: Colors.blue,
@@ -25,20 +25,46 @@ class _IdPageState extends State<IdPage> {
           ),
           child: Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+               Container(
+                 height: 50,
+                 width: double.infinity,
+                 decoration: BoxDecoration(
+                   color: Colors.black26,
+                   boxShadow: [
+                     BoxShadow(
+                       color: Colors.white,
+                       blurRadius: 5,
+                       blurStyle: BlurStyle.outer,
+                     )
+                   ]
+                 ),
+                 child: Center(
+                   child:  Text("ID CARD",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 25),),
+                 ),
+               ),
+                Padding(padding: EdgeInsets.only(bottom: 20)),
                 CircleAvatar(
                   radius: 90,
                   backgroundColor: Colors.black,
                   backgroundImage: (fileimg!=null)?FileImage(fileimg!):null,
                 ),
-                Text("First Name : "+fname,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
-                Text("Last Name : "+lname,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
-                Text("Address : "+add,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
-                Text("Email : "+email,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
-                Text("Phone No. : "+num,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
-                Text("BOD : "+date,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60,top: 10,),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("First Name : "+fname,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text("Last Name : "+lname,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text("Address : "+add,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text("Email : "+email,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text("Phone No. : "+num,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text("BOD : "+date,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700),),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
